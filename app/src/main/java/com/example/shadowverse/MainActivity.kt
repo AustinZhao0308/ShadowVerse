17e4c53ad7d6588a56a19f8d8bdfcb9a0313c05f
zhaochenkai <zhaochenkai@kuaishou.com>
2023-08-11 11:19:44 +0800

    update

--- app/src/main/java/com/example/shadowverse/MainActivity.kt
+++ app/src/main/java/com/example/shadowverse/MainActivity.kt
@@ -15,17 +15,7 @@ import com.example.shadowverse.ui.theme.ShadowVerseTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ShadowVerseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("ShadowVerse!")
-                }
-            }
-        }
+        setContentView(R.layout.main_activity)
     }
 }
 
